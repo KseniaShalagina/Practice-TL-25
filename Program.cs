45abe0b395a9d90e4377603f8aa3b90c2196f109
Ksenia Shalagina <kseniashalagina2803@gmail.com>
2025-08-01 12:50:31 +0300

    mistakes has been fixed in casino homework

--- Program.cs
+++ Program.cs
@@ -1,129 +1,131 @@
 ﻿using CasinoMy;
-const string GameName = @"
-   ####   ##    ####  ####  #  #  ####
-   #     #  #  #       ##   #  #  #  #
-   #     ####   ####   ##   ####  #  #
-   #     #  #      #   ##   #  #  #  #
-   ####  #  #  ####   ####  #  #  ####";
-
-PrintGameName(GameName);
-static void PrintGameName(string GameName)
+class Program
 {
-    Console.WriteLine(GameName);
-}
-Console.Write("Введите начальный баланс: ");
-string balanseStr = Console.ReadLine();
-bool isBanaseParsed = int.TryParse(balanseStr, out int balance);
-if (!isBanaseParsed)
-{
-    Console.WriteLine($"Не кореектно введен баланс: {balanseStr}");
-    return;
-}
-
-if (balance <= 0)
-{
-    Console.WriteLine("игра окончена, отрицательный баланс");
-    return;
-}
-Console.WriteLine($"Баланс: {balance}");
-
-const int multiplicator = 1;
-
-
-
-Operations? operation = Operations.Initial;
-
-while (operation != Operations.Exit&&operation!=null&&balance>0)
-{
-    operation = ReadOperations();
-    HendleOperation(operation.Value, ref balance);
-}
-static Operations? ReadOperations()
-{
-    Console.WriteLine("Menu");
-    Console.WriteLine("1 - Начать игру");
-    Console.WriteLine("2 - Проверить баланс");
-    Console.WriteLine("3 - Выход");
-    Console.Write("Ваш выбор: ");
-    string operationStr = Console.ReadLine();
-    bool isParsed = Enum.TryParse(operationStr, out Operations operations);
-
-    return isParsed ? operations : null;
-}
-
-static void HendleOperation(Operations operation, ref int balance)
-{
-
-    switch (operation)
+    static void Main( string[] args )
     {
-        case Operations.Initial:
-            return;
-        case Operations.Play:
-
-            Game(ref balance);
-            break;
-        case Operations.CheckBalance:
-            CheckBalanse(balance);
-            break;
-        case Operations.Exit:
-            Console.WriteLine("До свидания!");
-            break;
-        default:
-            throw new Exception($"unsuputtid operation: {operation}");
-            break;
+        const string GameName = @"
+    ####   ##    ####  ####  #  #  ####
+    #     #  #  #       ##   #  #  #  #
+    #     ####   ####   ##   ####  #  #
+    #     #  #      #   ##   #  #  #  #
+    ####  #  #  ####   ####  #  #  ####";
+        const int multiplicator = 1;
+        PrintGameName( GameName );
+        int balance = GetBalance();
+        Console.WriteLine( $"Баланс: {balance}" );
+        Operations? operation = Operations.Initial;
+        while ( operation != Operations.Exit && balance > 0 )
+        {
+            operation = ReadOperations();
+            balance = HandleOperation( operation.Value, balance, multiplicator );
+        }
     }
-}
-static bool CheckBalanse(int balance)
-{
-    Console.WriteLine($"Ваш баналс: {balance}");
-    if (balance > 0)
+    static int GetBalance()
     {
-        Console.WriteLine("Вы можете продолжать игру");
-        return true;
+        Console.Write( "Введите начальный баланс: " );
+        string balanceStr = Console.ReadLine();
+        bool isBanaseParsed = int.TryParse( balanceStr, out int balance );
+        if ( !isBanaseParsed )
+        {
+            Console.WriteLine( $"Не кореектно введен баланс: {balanceStr}" );
+            return 0;
+        }
+        else
+        {
+            return balance;
+        }
     }
-    else
+    static void PrintGameName( string GameName )
     {
-        Console.WriteLine("Ваш баланс отрицательный, для вас игра окончена");
-        return false;
+        Console.WriteLine( GameName );
     }
-}
-static int InputBet()
-{
-    Console.Write("Введите ставку: ");
-    string betStr = Console.ReadLine();
-    bool isBetParsed = int.TryParse(betStr, out int bet);
-    if (!isBetParsed || bet <= 0)
+    static Operations? ReadOperations()
     {
-        Console.WriteLine($"Не кореектно введен баланс: {betStr}");
-        throw new Exception($"Invalid bet value: {bet}");
-
+        Console.WriteLine( "Menu" );
+        Console.WriteLine( "1 - Начать игру" );
+        Console.WriteLine( "2 - Проверить баланс" );
+        Console.WriteLine( "3 - Выход" );
+        Console.Write( "Ваш выбор: " );
+        string operationStr = Console.ReadLine();
+        bool isParsed = Enum.TryParse( operationStr, out Operations operations );
+        return isParsed ? operations : null;
     }
-
-    Console.WriteLine($"Ваша ставка: {bet}");
-    return bet;
-}
-
-static int Game(ref int balance)
-{
-    int betGame = InputBet();
-    int number = RandomNumber();
-    Console.WriteLine($"Выпала фишка: {number}");
-    if (number < 18)
+    static int HandleOperation( Operations operation, int balance, int multiplicator )
     {
-        Console.WriteLine("Вы проиграли)");
-        balance = balance - betGame;
+        switch ( operation )
+        {
+            case Operations.Initial:
+                return balance;
+            case Operations.Play:
+                balance = PlayGame( balance, multiplicator );
+                break;
+            case Operations.CheckBalance:
+                CheckBalance( balance );
+                break;
+            case Operations.Exit:
+                Console.WriteLine( "До свидания!" );
+                break;
+            default:
+                Console.WriteLine( $"Unsuputtid operation: {operation}" );
+                break;
+        }
+        return balance;
     }
-    else
+    static int CheckBalance( int balance )
     {
-        Console.WriteLine("Вы выиграли!!!!");
-        int win = betGame * (multiplicator + (1 * number % 17));
-        balance = balance + win;
+        Console.WriteLine( $"Ваш баланс: {balance}" );
+        if ( balance > 0 )
+        {
+            Console.WriteLine( "Вы можете продолжать игру" );
+            return balance;
+        }
+        else
+        {
+            Console.WriteLine( "Ваш баланс отрицательный, для вас игра окончена" );
+            return 0;
+        }
     }
-    return balance;
-}
-static int RandomNumber()
-{
-    Random rnd = new Random();
-    var randomBetween = rnd.Next(0, 21);
-    return randomBetween;
+    static int InputBet()
+    {
+        while ( true )
+        {
+            Console.Write( "Введите ставку: " );
+            string betStr = Console.ReadLine();
+            bool isBetParsed = int.TryParse( betStr, out int bet );
+            if ( isBetParsed && bet > 0 )
+            {
+                Console.WriteLine( $"Ваша ставка: {bet}" );
+                return bet;
+            }
+            else
+            {
+                Console.WriteLine( $"Некорректно введен баланс: {betStr}" );
+                Console.WriteLine( $"Invalid bet value: {bet}" );
+            }
+        }
+    }
+    static int PlayGame( int balance, int multiplicator )
+    {
+        int betGame = InputBet();
+        int number = GetRandomNumber();
+        Console.WriteLine( $"Выпала фишка: {number}" );
+        if ( number < 18 )
+        {
+            Console.WriteLine( "Вы проиграли)" );
+            balance = balance - betGame;
+        }
+        else
+        {
+            Console.WriteLine( "Вы выиграли!!!!" );
+            int win = betGame * ( multiplicator + ( 1 * number % 17 ) );
+            balance = balance + win;
+        }
+        return balance;
+    }
+    static int GetRandomNumber()
+    {
+        Random rnd = new Random();
+        var randomBetween = rnd.Next( 0, 21 );
+        return randomBetween;
+    }
 }
